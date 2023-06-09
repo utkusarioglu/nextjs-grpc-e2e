4e2e469e6d1d3251531b13381c382629c09ea2f1
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-06-09 09:52:39 +0000

    Feat: Update tooling
    - Update cypress version and running container to the latest available,
      which is 12.13.0
    - Update devcontainer version to 20-slim.0.7
    - Update cypress run scripts for maintainability and for new tooling
      such as yarn. The container now mounts yarn dependencies such as
      `.yarnrc.yml`.
    - Update yarn to v3.
    - Add new functions to `test.utils.js` for handling the window size. Now
      there is some padding in every test runner to account for the size of
      the cypress ui elements in videos.
    - Remove `home.cy.js`. Tests in that file are outdated, that page
      doesn't exist anymore.
    - Add `login.cy.js`. This file currently tests the login keywords
      `error`, `wrong` and `guest`, which offer easy ways to simulate
      certain responses on the login screen. The tests in this file are not
      exhaustive.

--- .cypress/utils/test.utils.js
+++ .cypress/utils/test.utils.js
@@ -4,13 +4,23 @@ function describeName(Cypress, viewportSize, groupDefinition) {
   const groupString = groupDefinition ? ` - ${groupDefinition}` : "";
   return `${Cypress.browser.name} @ ${viewportSize.join("x")}${groupString}`;
 }
+
+/**
+ * Parses the video paddings from the environment file. The paddings are
+ * required to make room for cypress UI elements during tests; so that
+ * the tests pages are not scaled to less than 100% in chrome and other
+ * browsers.
+ */
+function getE2eVideoPaddings() {
+  return process.env.E2E_VIDEO_PADDING.split(",");
+}
 /**
  * Takes the max height and width from x and y of viewport sizes
  */
-function calculateE2eWindowSize(viewportSizes) {
+function calculateE2eWindowSize(viewportSizes, videoPadding) {
   return [
-    Math.max(...viewportSizes.map((sizes) => sizes[0])),
-    Math.max(...viewportSizes.map((sizes) => sizes[1])),
+    Math.max(...viewportSizes.map((sizes) => sizes[0])) + +videoPadding[0],
+    Math.max(...viewportSizes.map((sizes) => sizes[1])) + +videoPadding[1],
   ];
 }
 
@@ -35,4 +45,5 @@ module.exports = {
   calculateE2eWindowSize,
   getE2eBrowsers,
   getE2eViewportSizes,
+  getE2eVideoPaddings,
 };
--- scripts/docker-run-cypress.sh
+++ scripts/docker-run-cypress.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-WORKDIR=/utkusarioglu-com/projects/nextjs-grpc/e2e
-source "$WORKDIR/.env"
+# WORKDIR=/utkusarioglu-com/projects/nextjs-grpc/e2e
+WORKDIR=/cypress-workspace
+CYPRESS_VERSION=12.13.0
+
+if [ -z "$CI" ]; then
+  GITHUB_WORKSPACE=$(pwd)/..
+fi 
+
+echo "Using GITHUB_WORKSPACE: $GITHUB_WORKSPACE"
+
+if [ -z "$GITHUB_WORKSPACE" ]; then
+  echo "env.GITHUB_WORKSPACE is required to run this script"
+  exit 1
+fi
+
 repo_dir="$GITHUB_WORKSPACE/e2e"
+source "$repo_dir/.env"
 
 if [ ! -f $(/var/run/docker.sock) ];
 then
@@ -11,17 +25,25 @@ then
   exit 1
 fi
 
+echo "Using cypress/included:$CYPRESS_VERSION…"
+
 docker run \
   --rm \
+  --user 0:0 \
   -t \
   -v "$repo_dir/.env:$WORKDIR/.env" \
-  -v "$repo_dir/.cypress:$WORKDIR/cypress" \
-  -v "$repo_dir/scripts:$WORKDIR/scripts" \
-  -v "$repo_dir/cypress.config.js:$WORKDIR/cypress.config.js" \
   -v "$repo_dir/package.json:$WORKDIR/package.json" \
+  -v "$repo_dir/scripts:$WORKDIR/scripts" \
+  -v "$repo_dir/.cypress:$WORKDIR/cypress" \
+  -v "$repo_dir/cypress.config.js:$WORKDIR/cypress.config.js" \
+  -v "$repo_dir/.cypress/cache/node_modules:$WORKDIR/node_modules" \
+  -v "$repo_dir/.cypress/cache/cypress:/root/.cache/Cypress" \
+  -v "$repo_dir/.yarnrc.yml:$WORKDIR/.yarnrc.yml" \
+  -v "$repo_dir/.yarn:$WORKDIR/.yarn" \
+  -v "$repo_dir/yarn.lock:$WORKDIR/yarn.lock" \
   -w $WORKDIR \
   --network host \
   --name nextjs-grpc-e2e \
   --add-host "$BASE_URL:127.0.0.1" \
   --entrypoint scripts/run-cypress-tests.sh \
-  cypress/included:10.0.0 \
+  cypress/included:$CYPRESS_VERSION \
