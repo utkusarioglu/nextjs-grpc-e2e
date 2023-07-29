c4ba37c8527d2afbb99bc01d9d7cc8f1d907dd9c
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-07-29 07:46:45 +0000

    experiment/chore/infra-integration/6

--- scripts/prep-cypress-tests.sh
+++ scripts/prep-cypress-tests.sh
@@ -2,18 +2,22 @@
 
 LOGS_PATH=".cypress/artifacts/logs"
 YARN_LOGS_PATH=$LOGS_PATH/yarn.log
-# CA_PATH=/utkusarioglu-com/projects/nextjs-grpc/e2e/.certs/intermediate/ca.crt
 
 mkdir -p $LOGS_PATH
 touch $YARN_LOGS_PATH
 
 yarn --frozen-lockfile
 
+echo '<test code>'
+mkdir cypress/artifacts
 echo 'from prep file' > cypress/artifacts/prep.txt
+
 pwd
+ls
 ls cypress
 ls cypress/artifacts
 cat cypress/artifacts/prep.txt
+echo '</test code>'
 
 # echo '<Curl install>'
 # apt update && apt install -y curl
