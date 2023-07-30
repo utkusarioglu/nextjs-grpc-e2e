1698d6296da0727a99910f8c40abbca36249276f
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-07-30 08:46:10 +0000

    experiment/chore/infra-integration/14

--- scripts/run-cypress-tests.sh
+++ scripts/run-cypress-tests.sh
@@ -3,33 +3,11 @@
 #
 # scripts/prep-cypress-tests.sh needs to be run before this script
 #
-
-# LOGS_PATH=".cypress/artifacts/logs"
-# YARN_LOGS_PATH=$LOGS_PATH/yarn.log
 CA_PATH=/utkusarioglu-com/projects/nextjs-grpc/e2e/.certs/intermediate/ca.crt
-
-# mkdir -p $LOGS_PATH
-# touch $YARN_LOGS_PATH
-
-# yarn --frozen-lockfile
-# echo '<Curl install>'
-# apt update && apt install -y curl
-# echo '</Curl install>'
-# echo '<Curl test>'
-# curl --insecure https://nextjs-grpc.utkusarioglu.com
-# echo '</Curl test>'
 
 echo '<Certificate>'
 cat $CA_PATH
 echo '</ Certificate>'
-
-echo '<Curl, homepage>'
-curl http://nextjs-grpc.utkusarioglu.com | head -n1 -c80
-echo '</Curl, homepage>'
-
-echo '<Curl with ca, homepage>'
-curl https://nextjs-grpc.utkusarioglu.com | head -n1 -c80
-echo '</Curl with ca, homepage>'
 
 NODE_EXTRA_CA_CERTS=$CA_PATH scripts/run-cypress-tests.js
 
