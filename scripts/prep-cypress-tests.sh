8fc1915bfdd4edfacb0c47a96ba0d7b3226b24d5
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-07-30 08:05:20 +0000

    experiment/chore/infra-integration/13

--- scripts/prep-cypress-tests.sh
+++ scripts/prep-cypress-tests.sh
@@ -8,27 +8,4 @@ touch $YARN_LOGS_PATH
 
 yarn --frozen-lockfile
 
-echo '<test code>'
 mkdir cypress/artifacts
-echo 'from prep file' > cypress/artifacts/prep.txt
-
-pwd
-ls
-ls cypress
-ls cypress/artifacts
-cat cypress/artifacts/prep.txt
-echo '</test code>'
-
-# echo '<Curl install>'
-# apt update && apt install -y curl
-# echo '</Curl install>'
-# echo '<Curl test>'
-# curl --insecure https://nextjs-grpc.utkusarioglu.com
-# echo '</Curl test>'
-
-# echo '<Certificate>'
-# cat $CA_PATH
-# echo '</ Certificate>'
-
-# NODE_EXTRA_CA_CERTS=$CA_PATH \
-#   scripts/run-cypress-tests.js
