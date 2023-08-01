b28db67015b98fc96e9bd7b1dd9174bb6f19b857
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-08-01 13:30:29 +0000

    experiment/chore/infra-integration/15

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { defineConfig } = require("cypress");
 
 // const baseUrl = `https://${process.env.BASE_URL}`;
-const baseUrl = `http://${process.env.BASE_URL}`;
+const baseUrl = `https://${process.env.BASE_URL}`;
 
 module.exports = defineConfig({
   e2e: {
