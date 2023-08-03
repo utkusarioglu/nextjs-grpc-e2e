4360f65a3438f3aa42b8a93b0ca790e676748522
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-08-03 10:22:50 +0000

    Chore: Remove commented code
    - Remove commented code in `cypress.config.js`.

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const { defineConfig } = require("cypress");
 
-// const baseUrl = `https://${process.env.BASE_URL}`;
 const baseUrl = `https://${process.env.BASE_URL}`;
 
 module.exports = defineConfig({
